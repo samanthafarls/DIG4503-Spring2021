ef49d8f1d636d7d053f53dfa64f5538903949e67
samanthafarls <froggy4960@gmail.com>
2021-01-25 13:57:06 -0500

    lab1

---
+++ lab1/Samantha.js
@@ -0,0 +1,9 @@
+import Person from './Person.js';
+
+class Samantha extends Person{
+    constructor(){
+        super("Samantha", "#31D5E4");
+    }
+}
+
+export default Samantha;
---
+++ lab1/index.js
@@ -0,0 +1,5 @@
+import Person from './Person.js';
+import Samantha from './Samantha.js';
+
+const p = new Samantha ("Samantha", "#31D5E4");
+p.speak();
